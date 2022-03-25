286e325c0045456dde001ba630de3c97c63d272b
Mathantuness <102089949+Mathantuness@users.noreply.github.com>
2022-03-25 17:12:42 -0300

    Add files via upload
    task4

---
+++ exercicio.js
@@ -0,0 +1,101 @@
+//Estrutura Sequencial
+// Crie um script que leia o nome da pessoa e mostre o nome dela em um alert() e no console 
+/*
+let nome= prompt ("digite seu nome")
+alert("seu nome é;" + nome)
+console.log(nome) */
+
+
+
+// Crie um script que leia do usuário dois números e mostre a soma desses dois números.//
+/*
+let número= parseInt (prompt ("digite um número"))
+let número2= parseInt (prompt ("digite outro número"))
+console.log (número+número2) */
+
+//Crie um script que leia duas notas do usuário e calcule a média do aluno.
+    /*
+    let nota= parseInt(prompt("digite uma nota"))
+    let nota2= parseFloat(prompt("digite outra nota"))
+    x=nota+nota2
+    console.log(nota+nota2%2) */
+
+//	Crie um programa leia o ano em que o usuário nasceu e diga a idade atual dele.//
+/*
+let ano= prompt("digite o ano que vc nasceu")
+var resultado = 2022-ano
+alert("sua idade é:"+ resultado)
+console.log(resultado)
+*/
+
+//Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês./
+/*
+let sh= parseFloat (prompt ("qual seu salario por hora?"))
+let ht=parseInt (prompt("quantas horas vc trabalha no mês?"))
+total=sh*ht
+console.log(total) */
+//6.	Faça um script que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.    C = 5 * ((F-32) / 9).
+/*
+let GF= parseFloat (prompt("digite seus graus em Fahrenheit"))
+total=GF-32/1.8
+console.log(total) */
+
+// Estrutura de decisão
+
+/* 1.	Faça um Programa que peça dois números e imprima o maior deles.*/
+
+/* let numero1= parseInt(prompt ("digite um número"))
+let numero2= parseInt(prompt("digite outro número"))
+if (numero1>numero2) { 
+    console.log ("o numero1 é maior"+ numero1)
+} else{"o numero2 é maior" +numero2)
+console.log ("numero2 é maior" + numero2)
+ } */
+ /* 2.	Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.*/
+ /* let número1 = parseInt(prompt ("digite um número"))
+ if (numero1<0 ) {
+ console.log("o número é negativo")
+
+ } else { console.log ("o número é positivo") 
+
+ }
+*/ 
+
+
+//3.	Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+/*let sexo= prompt ("digite seu sexo")
+if(=="m") {
+    console.log("sexo masculino")    
+} else (=="f")
+{
+    console.log("sexo feminino")
+} */
+
+/* 4.	Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+1.	A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+2.	A mensagem "Reprovado", se a média for menor do que sete;
+3.	A mensagem "Aprovado com Distinção", se a média for igual a dez. */
+/* let nota1 = parseInt (prompt ("digite sua primeira nota"))
+let nota2 = parseInt (prompt ("digite sua segunda nota"))
+console.log(nota1+nota2&2)
+if{
+console.log ("aprovado">=7)
+
+}
+else{
+  console.log  ("reprovado"<7)
+}
+else{
+    console.log("aprovado com distinção"=10 )
+} */
+// 5.	Faça um Programa que leia três números e mostre o maior e o menor deles. //
+/* let numero1= parseInt (prompt ("digite um número")
+let numero2= parseInt(prompt("digite outro numero"))
+if{ 
+    console.log  (numero1>número2)
+
+}
+else{
+    console.log(numero2 >numero1)
+} */
+
